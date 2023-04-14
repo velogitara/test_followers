5a98b60396e27e2cec6a910403da49ed324ac792
Alex Pavlishchev <velogitara@gmail.com>
2023-04-14 12:23:35 +0300

    add routers

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "https://velogitara.github.io/test_followers/",
-    plugins: [react()],
+  base: 'https://velogitara.github.io/',
+  plugins: [react()],
 });
